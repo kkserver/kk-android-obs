f502f1cf73bf454925471cd1a44fb9409ec76e7a
zhang hailong <hailongz@qq.com>
2016-11-08 21:03:08 +0800

    update

--- kk-obs/src/main/java/cn/kkserver/observer/IGetter.java
+++ kk-obs/src/main/java/cn/kkserver/observer/IGetter.java
@@ -3,7 +3,7 @@ package cn.kkserver.observer;
 /**
  * Created by zhanghailong on 16/7/13.
  */
-public interface IGetter {
+public interface IGetter extends cn.kkserver.core.IGetter{
 
     public java.lang.Object get(String key);
 
--- kk-obs/src/main/java/cn/kkserver/observer/ISetter.java
+++ kk-obs/src/main/java/cn/kkserver/observer/ISetter.java
@@ -3,7 +3,7 @@ package cn.kkserver.observer;
 /**
  * Created by zhanghailong on 16/7/13.
  */
-public interface ISetter {
+public interface ISetter extends cn.kkserver.core.ISetter {
 
     public void set(String key, java.lang.Object value);
 
